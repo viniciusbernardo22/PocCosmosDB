ce00b2bd32efab0f114fb3e90e8c91aefceb2ff6
viniciusbernardo22 <vini383@gmail.com>
2024-07-08 16:24:40 -0300

    feat: add gitignore

--- src/PocCosmosDb.BlazorServer/obj/Debug/net6.0/PocCosmosDb.BlazorServer.AssemblyInfo.cs
+++ src/PocCosmosDb.BlazorServer/obj/Debug/net6.0/PocCosmosDb.BlazorServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PocCosmosDb.BlazorServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70f41624a72f26320d812426a54b9e84eb6ffb2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PocCosmosDb.BlazorServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PocCosmosDb.BlazorServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
